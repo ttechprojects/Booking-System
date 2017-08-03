eceec36d538f2577a2c994cb7ad5355dd1e850d8
ttechprojects <30691520+ttechprojects@users.noreply.github.com>
2017-08-03 15:10:57 +0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="stylesheet/color.css">
+    </head>
+    <body>
+    <form method="post" action="" >
+        <img id="logo" src="images/Emirates_logo.svg.png">
+        <input type="text" name="username" placeholder="Username" class="input">
+        <input type="password" name="password" placeholder="Password" class="input"><br><br><br>
+        <a href="forgotpwd.php">Forgot Password?</a><br>
+        <input type="submit" name="Submit" class="button">
+    </form>
+    </body>
+
+</html>
