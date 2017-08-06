a6e2513fd863dc6150cccff55e7a0c546e64c11e
ttechprojects <30691520+ttechprojects@users.noreply.github.com>
2017-08-06 11:45:37 +0400

    Add files via upload

--- login.php
+++ login.php
@@ -4,12 +4,24 @@
     <link rel="stylesheet" type="text/css" href="stylesheet/color.css">
     </head>
     <body>
-    <form method="post" action="" >
+    <form method="post" action="auth.php" >
         <img id="logo" src="images/Emirates_logo.svg.png">
         <input type="text" name="username" placeholder="Username" class="input">
         <input type="password" name="password" placeholder="Password" class="input"><br><br><br>
         <a href="forgotpwd.php">Forgot Password?</a><br>
         <input type="submit" name="Submit" class="button">
+        <div style="text-align:center; font-family:helvetica;"> 
+            <!---Fix styling for error message--->
+        <?php 
+            session_start();
+            if(isset($_SESSION['Message']))
+            {
+                $string= $_SESSION['Message'];
+                echo $string;
+                unset($_SESSION['Message']);
+            }
+            ?>
+        </div>
     </form>
     </body>
 
