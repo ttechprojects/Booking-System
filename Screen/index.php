294ab920d7436397b0ec0a774954d173e460a6bb
ttechprojects <30691520+ttechprojects@users.noreply.github.com>
2017-08-28 16:01:06 +0400

    Add files via upload

---
+++ Screen/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+    </head>
+    
+    <body >
+        <style>
+            body{
+                margin:0 0 0 0;
+            }
+            #m_name{
+                margin-top: 11.5%;
+                font-size: 60px;
+            }
+            .text{
+                font-family: helvetica;
+                text-align: center;
+            }
+            .time{
+                font-family: helvetica;
+                text-align: center;
+                font-size: 30px;
+            }
+            
+            #logo{
+                float: right;
+            }
+        
+        </style>
+        
+        <p class="text" id="m_name">Meeting Title</p>
+        <p class="text time">Meeting Room Name</p>
+        <p class="text time">Start Time - End Time</p>
+        <img id="logo" src="logo.jpg">
+    </body>
+</html>
